44421973dae91b03e6fdbe5e9c5c2faa5b6d1ae0
Winter-Is-Coming-Stark <78529236+Winter-Is-Coming-Stark@users.noreply.github.com>
2021-07-17 01:09:46 +0800

    Add files via upload

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,3 +1,4 @@
+extern crate rand;
 
 mod Color;
 mod camera;
@@ -30,6 +31,7 @@ use sphere::Sphere;
 use std::collections::hash_map::Entry::Vacant;
 use std::rc::Rc;
 pub use vec3::Vec3;
+use crate::rtweekend::clamp;
 
 fn ray_color(r: Ray, world: &Hittable_list, depth: i32) -> color {
     let mut rec = hit_record::new();
@@ -72,89 +74,12 @@ fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
 
 fn main() {
     //image
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width: f64 = 400.0;
+    let aspect_ratio = 3.0 / 2.0;
+    let image_width: f64 = 1200.0;
     let image_height: f64 = image_width / aspect_ratio;
-    let samples_per_pixel = 100.0;
+    let samples_per_pixel = 500.0;
     let max_depth = 50;
 
-    //world
-    let mut world = Hittable_list::new_default();
-
-    let ground_material: Rc<Lambertian> = Rc::new(Lambertian::new(color::new(0.5, 0.5, 0.5)));
-    world.add(Rc::new(Sphere::new(
-        Point3::new(0.0, -1000.0, 0.0),
-        1000.0,
-        ground_material.clone(),
-    )));
-
-    let mut rng = rand::thread_rng();
-
-    let mut a = -11.0;
-    while a < 11.0{
-        let mut b = -11.0;
-        while b < 11.0{
-            let choose_mat = rng.gen::<f64>();
-            let center = Point3::new(a + 0.9 * rng.gen::<f64>(),0.2,b + 0.9 * rng.gen::<f64>());
-
-            if (center - Point3::new(4.0,0.2,0.0)).length() > 0.9{
-                if choose_mat < 0.8{
-                    let albedo = color::random() * color::random();
-                    let sphere_material = Rc::new(Lambertian::new(albedo));
-                    world.add(Rc::new(Sphere::new(
-                        center,
-                        0.2,
-                        sphere_material.clone(),
-                    )));
-                }
-
-                else if choose_mat < 0.95{
-                    let albedo = color::random_range(0.5,1.0);
-                    let fuzz = rng.gen_range(0.0..0.5);
-                    let sphere_material = Rc::new(Metal::new(albedo,fuzz));
-                    world.add(Rc::new(Sphere::new(
-                        center,
-                        0.2,
-                        sphere_material.clone(),
-                    )));
-                }
-
-                else{
-                    let sphere_material = Rc::new(Dielectric::new(1.5));
-                    world.add(Rc::new(Sphere::new(
-                        center,
-                        0.2,
-                        sphere_material.clone(),
-                    )));
-                }
-            }
-            b = b + 1.0;
-        }
-        a = a + 1.0;
-    }
-
-    let material1 = Rc::new(Dielectric::new(1.5));
-    world.add(Rc::new(Sphere::new(
-        Point3::new(0.0,1.0,0.0),
-        1.0,
-        material1.clone(),
-    )));
-
-    let material2 = Rc::new(Lambertian::new(color::new(0.4,0.2,0.1)));
-    world.add(Rc::new(Sphere::new(
-        Point3::new(-4.0,1.0,0.0),
-        1.0,
-        material2.clone(),
-    )));
-
-    let material3 = Rc::new(Metal::new(color::new(0.7,0.6,0.5),0.0));
-    world.add(Rc::new(Sphere::new(
-        Point3::new(4.0,1.0,0.0),
-        1.0,
-        material3.clone(),
-    )));
-
-    //let R = (pi / 4.0).cos();
     /*
     let mut world = Hittable_list::new_default();
 
@@ -189,7 +114,7 @@ fn main() {
         material_right.clone(),
     )));
     */
-
+    let world = random_scene();
     //camera
     let aspect_ratio = 16.0 / 9.0;
     let viewport_height = 2.0;
@@ -221,7 +146,10 @@ fn main() {
 
     //render
     println!("P3\n{} {} \n255\n", image_width, image_height);
+    let mut img: RgbImage = ImageBuffer::new(image_width as u32, image_height as u32);
+    let bar = ProgressBar::new(image_width as u64);
     let mut j = image_height - 1.0;
+
     while j >= 0.0 {
         let mut i = 0.0;
         while i < image_width {
@@ -234,11 +162,33 @@ fn main() {
                 pixel_color += ray_color(r, &world, max_depth);
                 s = s + 1.0;
             }
+
+            let pixel = img.get_pixel_mut(i as u32,j as u32);
+            let mut r = pixel_color.x;
+            let mut g = pixel_color.y;
+            let mut b = pixel_color.z;
+            let scale = 1.0 / samples_per_pixel;
+            r = (scale * r).sqrt();
+            g = (scale * g).sqrt();
+            b = (scale * b).sqrt();
+            clamp(r, 0.0, 0.999);
+            clamp(g, 0.0, 0.999);
+            clamp(b, 0.0, 0.999);
+            let r = r * 255.999;
+            let g = g * 255.999;
+            let b = b * 255.999;
+            let r = r as i64;
+            let g = g as i64;
+            let b = b as i64;
             color::wrt_color(&pixel_color, samples_per_pixel);
+            *pixel = image::Rgb([r as u8,g as u8,b as u8]);
             i = i + 1.0;
         }
+        bar.inc(1);
         j = j - 1.0;
     }
+    img.save("output/test.png").unwrap();
+    bar.finish();
 }
 
 pub fn random_scene() -> Hittable_list{
