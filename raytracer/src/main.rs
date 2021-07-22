129763c58b90bfdf51e3b8f31d6413c35f79cf9d
Winter-Is-Coming-Stark <78529236+Winter-Is-Coming-Stark@users.noreply.github.com>
2021-07-22 20:18:09 +0800

    Update main.rs

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -75,7 +75,7 @@ fn main() {
     let aspect_ratio = 1.0;
     let image_width: f64 = 800.0;
     let image_height: f64 = image_width / aspect_ratio;
-    let samples_per_pixel = 5000.0;
+    let samples_per_pixel = 10000.0;
     let max_depth = 50;
 
     //world
